9198ec9a688d0f46ab7318fb683fd758165789fd
Khushboo Vijayvargiya <Khushboo.Vijayvargiya@PTPL.Local>
2020-02-13 18:22:21 +0530

    strings,logo changes

--- resources/views/knitter/measurements/add-measurement.blade.php
+++ resources/views/knitter/measurements/add-measurement.blade.php
@@ -145,7 +145,7 @@
   <div class="modal-dialog modal-lg">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">Confirm Your Measurements</h5>
+        <h5 class="modal-title" id="exampleModalLongTitle">Confirm Your Measurements ffjh</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
@@ -154,7 +154,7 @@
         
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Edit</button>
         <button type="button" onclick="savedata();" class="btn theme-btn btn-primary waves-effect waves-light">Submit</button>
       </div>
     </div>
